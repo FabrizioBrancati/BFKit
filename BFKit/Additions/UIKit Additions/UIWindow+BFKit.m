46af9ec2c9706fc65773c48f928baabe1cf950c6
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2014-11-29 15:25:45 +0100

    Added UIWindow category

---
+++ BFKit/Additions/UIKit Additions/UIWindow+BFKit.m
@@ -0,0 +1,62 @@
+//
+//  UIWindow+BFKit.m
+//  BFKitDemo
+//
+//  Created by Fabrizio on 29/11/14.
+//  Copyright (c) 2014 Fabrizio Brancati. All rights reserved.
+//
+
+#import "UIWindow+BFKit.h"
+
+@implementation UIWindow (BFKit)
+
+- (UIImage *)takeScreenshot
+{
+    // Source (Under MIT License): https://github.com/shinydevelopment/SDScreenshotCapture/blob/master/SDScreenshotCapture/SDScreenshotCapture.m#L35
+    
+    BOOL ignoreOrientation = SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0");
+    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    
+    CGSize imageSize = CGSizeZero;
+    if (UIInterfaceOrientationIsPortrait(orientation) || ignoreOrientation) {
+        imageSize = [UIScreen mainScreen].bounds.size;
+    } else {
+        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+    }
+    
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, self.center.x, self.center.y);
+    CGContextConcatCTM(context, self.transform);
+    CGContextTranslateCTM(context, -self.bounds.size.width * self.layer.anchorPoint.x, -self.bounds.size.height * self.layer.anchorPoint.y);
+    
+    // correct for the screen orientation
+    if (!ignoreOrientation) {
+        if (orientation == UIInterfaceOrientationLandscapeLeft) {
+            CGContextRotateCTM(context, (CGFloat)M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+        } else if (orientation == UIInterfaceOrientationLandscapeRight) {
+            CGContextRotateCTM(context, (CGFloat)-M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+            CGContextRotateCTM(context, (CGFloat)M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+        }
+    }
+    
+    if([self respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
+        [self drawViewHierarchyInRect:self.bounds afterScreenUpdates:NO];
+    } else {
+        [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    }
+    
+    CGContextRestoreGState(context);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+@end
