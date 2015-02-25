b402d486681a20abcb7d4172442575a4433ef4d9
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-02-25 02:21:07 +0100

    Minor changes

--- BFKit/Additions/Foundation/NSDictionary+BFKit.m
+++ BFKit/Additions/Foundation/NSDictionary+BFKit.m
@@ -28,21 +28,28 @@
 
 @implementation NSDictionary (BFKit)
 
-- (NSString *)dictionaryToJson {
+- (NSString *)dictionaryToJson
+{
     return [NSDictionary dictionaryToJson:self];
 }
 
-+ (NSString *)dictionaryToJson:(NSDictionary *)dictionary {
-    NSString *json     = nil;
-    NSError  *error    = nil;
-    NSData   *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
-
-    if (!jsonData) {
++ (NSString *)dictionaryToJson:(NSDictionary *)dictionary
+{
+    NSString *json = nil;
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
+    
+    if(!jsonData)
+    {
         return @"{}";
-    } else if (!error) {
+    }
+    else if(!error)
+    {
         json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
         return json;
-    } else {
+    }
+    else
+    {
         return error.localizedDescription;
     }
 }
