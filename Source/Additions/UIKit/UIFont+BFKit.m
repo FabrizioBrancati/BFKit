acbc81fbc7a43fcd6f8d3b6e163690975aca5e6e
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2015-12-24 11:03:12 +0100

    Changed how to access lightFont, regularFont and boldFont in UIFont category

--- Source/Additions/UIKit/UIFont+BFKit.m
+++ Source/Additions/UIKit/UIFont+BFKit.m
@@ -4,7 +4,7 @@
 //
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2014 - 2015 Fabrizio Brancati. All rights reserved.
+//  Copyright (c) 2014 - 2016 Fabrizio Brancati. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -29,6 +29,34 @@
 #import "BFLog.h"
 
 @implementation UIFont (BFKit)
+
+static UIFont * _Nullable lightFont;
+static UIFont * _Nullable regularFont;
+static UIFont * _Nullable boldFont;
+
++ (UIFont *)lightFont {
+    return UIFont.lightFont;
+}
+
++ (void)setLightFont:(UIFont *)newLightFont {
+    UIFont.lightFont = newLightFont;
+}
+
++ (UIFont *)regularFont {
+    return UIFont.regularFont;
+}
+
++ (void)setRegularFont:(UIFont *)newRegularFont {
+    UIFont.regularFont = newRegularFont;
+}
+
++ (UIFont *)boldFont {
+    return UIFont.boldFont;
+}
+
++ (void)setBoldFont:(UIFont *)newBoldFont {
+    UIFont.boldFont = newBoldFont;
+}
 
 + (NSDictionary * _Nonnull)allFamilyAndFonts {
     NSMutableArray *fontFamilies = (NSMutableArray *)[UIFont familyNames];
