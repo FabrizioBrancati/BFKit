7c29f4c6f2bb1a4f50acecf7f7abe36ff57c9a10
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-02-01 01:15:07 +0100

    Added screen size macros

--- Source/Additions/UIKit/UIScreen+BFKit.h
+++ Source/Additions/UIKit/UIScreen+BFKit.h
@@ -33,6 +33,21 @@
 #define SCREEN_HEIGHT [[UIScreen mainScreen] fixedScreenSize].height
 
 /**
+ *  Get the maximum & mininum screen length
+ */
+#define SCREEN_MAX_LENGTH MAX(SCREEN_WIDTH, SCREEN_HEIGHT)
+#define SCREEN_MIN_LENGTH MIN(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+/**
+ *  A block of macros that returns a BOOL to check the screen size
+ */
+#define IS_IPHONE_4_OR_LESS [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && SCREEN_MAX_LENGTH < 568.0
+#define IS_IPHONE_5 [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && SCREEN_MAX_LENGTH == 568.0
+#define IS_IPHONE_6 [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && SCREEN_MAX_LENGTH == 667.0
+#define IS_IPHONE_6P [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone && SCREEN_MAX_LENGTH == 736.0
+#define IS_IPAD [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad && SCREEN_MAX_LENGTH == 1024.0
+
+/**
  *  This category adds some useful methods to UIScreen
  *
  *  SCREEN_WIDTH: Get the screen width
