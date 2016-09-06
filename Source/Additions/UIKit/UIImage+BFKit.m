ae0060912ec4d5115ae7af10ef3d0ea613bb4801
Fabrizio Brancati <fabrizio.brancati@gmail.com>
2016-09-06 22:53:07 +0200

    Fixed #26 imageAtRect:(CGRect)rect

--- Source/Additions/UIKit/UIImage+BFKit.m
+++ Source/Additions/UIKit/UIImage+BFKit.m
@@ -143,8 +143,14 @@
 }
 
 - (UIImage * _Nonnull)imageAtRect:(CGRect)rect {
-    CGImageRef imageRef = CGImageCreateWithImageInRect([self CGImage], rect);
-    UIImage *subImage = [UIImage imageWithCGImage:imageRef];
+    CGRect realRect;
+    if (rect.origin.x != 0 && rect.origin.y != 0) {
+        realRect = CGRectMake(0, 0, rect.size.width * self.scale, rect.size.height * self.scale);
+    } else {
+        realRect = CGRectMake(rect.origin.x * self.scale, rect.origin.y * self.scale, rect.size.width * self.scale, rect.size.height * self.scale);
+    }
+    CGImageRef imageRef = CGImageCreateWithImageInRect([self CGImage], realRect);
+    UIImage *subImage = [UIImage imageWithCGImage:imageRef scale:self.scale orientation:self.imageOrientation];
     CGImageRelease(imageRef);
     
     return subImage;
